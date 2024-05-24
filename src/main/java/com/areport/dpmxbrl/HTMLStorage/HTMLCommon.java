5bc7d6aa5d626981731fba08490ca0babeda9178
Munnolli <munnollishanmukh@gmail.com>
2024-05-24 19:38:56 +0530

    mavenproject

---
+++ src/main/java/com/areport/dpmxbrl/HTMLStorage/HTMLCommon.java
@@ -0,0 +1,9 @@
+package com.areport.dpmxbrl.HTMLStorage;
+
+public class HTMLCommon {
+
+    public HTMLCommon(String attributes, int tabOffset) {
+        //TODO Auto-generated constructor stub
+    }
+
+}
---
+++ src/main/java/com/areport/dpmxbrl/HTMLStorage/HTMLTableStorage.java
@@ -0,0 +1,20 @@
+package com.areport.dpmxbrl.HTMLStorage;
+
+public class HTMLTableStorage {
+
+    public void setUseTGroups(boolean b) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setUseTGroups'");
+    }
+
+    public void setAutoFill(String autoFill) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setAutoFill'");
+    }
+
+    public void setAttributes(String attributes) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setAttributes'");
+    }
+
+}
